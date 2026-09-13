d45da04a4eb8b5f8fee7847577a57f2e69a58a54
Thanushan925 <80972937+Thanushan925@users.noreply.github.com>
2026-09-13 11:57:44 -0400

    Setup PostgreSQL Backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import pool from "./db";
 
 const app = express();
 const PORT = 3000;
@@ -15,4 +16,22 @@ app.get("/api/health", (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Backend running at http://localhost:${PORT}`);
+});
+
+app.get("/api/db-test", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+
+    res.json({
+      status: "ok",
+      databaseTime: result.rows[0].now,
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      status: "error",
+      message: "Database connection failed.",
+    });
+  }
 });
